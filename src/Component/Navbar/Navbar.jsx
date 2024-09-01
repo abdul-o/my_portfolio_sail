856b82dcfe85efffde4f94d80e91037f626f9871
Abdul-o <abdullateefdauda01@gmail.com>
2024-09-01 11:23:23 +0100

    nav setup

---
+++ src/Component/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import logo from '../../assets/logo.jpeg'
+
+const Navbar = () => {
+  return (
+    <div>
+        <nav>
+            <div>
+                <img src={logo} alt="" />
+            </div>
+            <div></div>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
